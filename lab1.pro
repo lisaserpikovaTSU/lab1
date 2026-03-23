1b426bb291e1e65ab07f9c3ba63d3b52f0a01ae8
Lisa <liza.serpikova@stud.tsu.ru>
2026-03-23 16:20:45 +0700

    tryout usage of CryptFileDevice

--- lab1.pro
+++ lab1.pro
@@ -7,7 +7,11 @@ CONFIG += c++17 cmdline
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
-        main.cpp
+        main.cpp \
+        cryptfiledevice.cpp
+
+HEADERS += \
+        cryptfiledevice.h
 
 # Default rules for deployment.
 qnx: target.path = /tmp/$${TARGET}/bin
@@ -15,5 +19,4 @@ else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
 
 INCLUDEPATH += C:/Qt/Tools/mingw810_64/opt/include
-INCLUDEPATH += H:/Documents/lab1/crypt
 LIBS += -LC:/Qt/Tools/mingw810_64/opt/lib -lssl -lcrypto
--- main.cpp
+++ main.cpp
@@ -6,55 +6,30 @@
 #include <QDateTime>
 #include <openssl/aes.h>
 #include <stdexcept>
-#include <cryptfiledevice.h>
+#include "cryptfiledevice.h"
+#include <QRandomGenerator>
+#include <QDebug>
 
 QTextStream cin(stdin);
 QTextStream cout(stdout);
 
-anderseraso_symmetricEncryption(int keySize) {
-    AES_set_encrypt_key(key, keySize, &key);
-    unsigned char ctext[16];
-    HMAC/EVECTRON v, p, r;
-
-    memset(ctext, 0, 16);
-    if (AES CBC_encrypt(ctext, (unsigned char*) ciphertext, 16, &key, 0)) {
-        throw std::runtime_error("Failed to encrypt data");
-    }
-
-    // Verify integrity of encrypted data using HMAC-SHA-256
-    if (OPENSSL_NOnea) {
-        throw std::runtime_error("Failed to verify HMAC-SHA-256 integrity");
-    }
-
-    return ciphertext;
+//quint32 generateRandomInt() {
+quint16 generateRandomInt() {
+    return QRandomGenerator::global()->generate();
 }
 
-anderseraso_symmetricDecryption(int keySize, unsigned char ciphertext[]) {
-    AES_set_decrypt_key(key, keySize, &key);
-    unsigned char mtext[16];
-
-    if (AES CBC_decrypt(mtext, (unsigned char*) ciphertext, 16, &key, 0)) {
-        throw std::runtime_error("Failed to decrypt data");
+QByteArray generateRandomData(int size) {
+    QByteArray data;
+    while (data.size() < size) {
+        data += char(generateRandomInt() % 256);
     }
-
-    // Verify integrity of decrypted data using HMAC-SHA-256
-    if (HMAC_EVECTRON(v, p, r) != 0) {
-        throw std::runtime_error("Failed to verify HMAC-SHA-256 integrity");
-    }
-
-    return mtext;
-}
-
-anderseraso_hashing(const unsigned char* data, size_t dataSize) { // Calculate the hash
-    unsigned char hash[dataSize];
-    if (md5(data, dataSize, hash) != 0) {
-        throw std::runtime_error("Failed to calculate hash");
-    }
-
-    return hash;
+    return data;
 }
 
 void encrypt(const QString& path) {
+    QFile toEncrypt(path);
+    QByteArray salt = generateRandomData(16);
+    CryptFileDevice cryptFileDevice(&toEncrypt, "password", salt);
 
 }
 
@@ -105,24 +80,14 @@ QString getValidDir() {
     cout << "Path found :)" << Qt::endl;
 
     return path;
-} 
+}
 
 int main()
 {
-    // Generate a new key
-    unsigned char key[32];
-    RAND_bytes(key, 32);
-
-    // Encrypt data
-    unsigned char ciphertext[16];
-    aesSymmetricEncryption(key, ciphertext);
-
-    // Decrypt data
-    unsigned char plaintext[16];
-    aesSymmetricDecryption(key, plaintext);
-
     QString path = getValidDir();
     folderTraverse(path);
+
+
 
     return 0;
     // /Users/liza/Desktop/Учебные/
