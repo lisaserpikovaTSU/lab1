64c80f18b9c5452e7973ddab03e7ff8605b5c4b1
lisa <liza.serpikova@stud.tsu.ru>
2026-05-11 19:55:48 +0700

    deleted test comments

--- main.cpp
+++ main.cpp
@@ -16,53 +16,6 @@
 QTextStream cin(stdin);
 QTextStream cout(stdout);
 
-//Тестирование функции ШИФРОВАНИЯ папки (encryptFolder)
-
-//Входные данные - путь до папки, пароль
-
-//Случай 1: Папки по пути не существует
-//   Путь для проверки: /Users/liza/lab1/tests/nonexistingfolder
-
-//Случай 2: Папка по переданному пути существует и пуста
-//   Путь для проверки: /Users/liza/lab1/tests/emptyfolder
-
-//Случай 3: Папка по переданному пути существует, содержит
-//          подпапки и файлы
-//   Путь для проверки: /Users/liza/lab1/tests/subdirsandfiles
-
-//Случай 4: Папка содержит файлы разных типов
-//   Путь для проверки: /Users/liza/lab1/tests/differentfiles
-
-//Случай 5: Папка содержит пустые файлы
-//   Путь для проверки: /Users/liza/lab1/tests/emptyfiles
-
-//Случай 6: Папка является системной
-//   Путь для проверки: /System/Library/Siri/DM/SiriSuggestions/Owners
-
-//Случай 7: Папка содержит уже зашифрованные файлы
-//   Путь для проверки: /Users/liza/lab1/tests/encrypted
-
-//Случай 8: Пустой пароль
-//   Путь для проверки: /Users/liza/lab1/tests/subdirsandfiles
-//   Пароль: ""
-
-//Тестирование функции ДЕШИФРОВАНИЯ папки (decryptFolder)
-
-//Входные данные - путь до папки, пароль
-
-//Случай 9: Папка содержит зашифрованные файлы, введен верный пароль
-//   Путь для проверки: /Users/liza/lab1/tests/encrypted
-
-//Случай 10: Папка содержит незашифрованные файлы
-//   Путь для проверки: /Users/liza/lab1/tests/subdirsandfiles
-
-//Случай 11: Неверный пароль
-//   Путь для проверки: /Users/liza/lab1/tests/encrypted
-//   Пароль: "wrongpassword"
-
-//Случай 12: Пустой ввод пути
-//   Путь для проверки: ""
-
 int main()
 {
     QString path;
